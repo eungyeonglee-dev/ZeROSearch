6bc7d8edfbcfd017c9608d7ce94deadafa0b0d3f
eungyeong lee <anhanmu78@gmail.com>
2024-11-28 18:26:15 +0900

    add run shell

---
+++ Megatron-DeepSpeed/_00_conf_homo_A10_gpu2.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+CONTAINER_IMAGE_PATH="../container/deepspeed.2406.sqsh"
+CONTAINER_NAME="deepspeed2406"
+CONTAINER_PATH=/enroot/$UID/data/$CONTAINER_NAME
+
+TP=$1
+PP=$2
+DP=$3
+PARTITION=$4
+ZERO_STAGE=$5
+
+MODEL="llama2-13B"
+NNODES=8
+NPROC_PER_NODE=4
+
+MICRO_BATCH_SIZE=1
+NUM_MB=1
+# GLOBAL_BATCH_SIZE=64
+GLOBAL_BATCH_SIZE=$((MICRO_BATCH_SIZE * NUM_MB * DP))
+# L: llama2-13B:40
+
+NSYS=false
+PROFILE=true
+
+MASTER_PORT=7898
+RELOAD_CONTAINER=false
+
+OVERLAP=false
+
+PIPELINE_MODEL_PARALLEL_SPLIT_RANK=2 # Where the encoder ends within the pipeline group
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+echo "MBS: $MICRO_BATCH_SIZE"
+echo "TP: $TP"
+echo "PP: $PP"
+echo "DP: $DP"
+echo "PARTITION: $PARTITION"
+echo ZERO: $ZERO_STAGE
+
+# set model specific arguments
+echo "MODEL : $MODEL"
+if [ $MODEL == "llama2-13B" ]; then # LLAMA2 13B
+        HIDDEN_SIZE=5120
+        FFN_HIDDEN_SIZE=13824
+        NUM_LAYERS=40
+        NUM_HEADS=40
+        SEQ_LENGTH=4096
+        NUM_KV_HEADS=8
+        LR=3e-4
+        MIN_LR=3e-5
+        LR_WARMUP_STEPS=1
+        WEIGHT_DECAY=0.1
+        GRAD_CLIP=1
+else
+        echo error: invalid model argument MODEL only "llama2-13B" is allowed
+        return 1
+fi
+
+# set zero arguments
+if [ "$ZERO_STAGE" == "0" ]; then
+        echo "ZERO OFF"
+else
+        echo "ZERO STAGE: $ZERO_STAGE"
+fi
+
+# set overlap arguments
+if $OVERLAP; then
+        echo "OVERLAP: $OVERLAP"
+else
+        echo "OVERLAP OFF"
+fi
+
+# set profiling arguments
+if $PROFILE; then
+        echo "Profiling"
+        PROFILE_ARGS="--timing-log-level 2 --timing-log-option all"
+else
+        echo "Not profiling"
+        PROFILE_ARGS=""
+fi
+
+# set nsight arguments
+if $NSYS; then
+        echo "NSYS: $NSYS"
+else
+        echo "NSYS OFF"
+fi
---
+++ Megatron-DeepSpeed/_03_submit.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+TP=$1
+PP=$2
+DP=$3
+PARTITION=$4
+ZERO_STAGE=$5
+# NUM_HETERO == 1 GPU TYPE, NUM_HETERO == 2 A GPU TYPE
+A_GPU_TYPE=$6 # A10, A6000
+NUM_HETERO=$7 # the number of heterogeneous nodes
+
+if  [ $NUM_HETERO == "1" ]; then
+    . _00_conf_homo_${A_GPU_TYPE}.sh $TP $PP $DP $PARTITION $ZERO_STAGE
+    sbatch_job="_02_sbatch_E$A_GPU_TYPE.sh"
+    echo $sbatch_job
+    sbatch $sbatch_job $TP $PP $DP $PARTITION $ZERO_STAGE
+    echo "$A_GPU_TYPE homogeneous"
+
+elif [ $NUM_HETERO == "2" ]; then
+    . _00_conf_hetero2.sh $TP $PP $DP $PARTITION
+    sbatch _02_sbatch_hetero2_$A_GPU_TYPE.sh $TP $PP $DP $PARTITION
+    sbatch _02_sbatch_hetero2_RTX3090.sh $TP $PP $DP $PARTITION
+    
+elif [ $NUM_HETERO == "3" ]; then
+    . _00_conf.sh $TP $PP $DP $PARTITION
+    ARUN_FILE="ARUN.txt"
+    BRUN_FILE="BRUN.txt"
+    CRUN_FILE="CRUN.txt"
+
+    if [ -e $ARUN_FILE ]; then 
+        rm $ARUN_FILE
+        echo "remove $ARUN_FILE" 
+    fi
+    
+    if [ -e $BRUN_FILE ]; then
+        rm $BRUN_FILE 
+        echo "remove $BRUN_FILE"
+    fi
+
+    if [ -e $CRUN_FILE ]; then
+        rm $CRUN_FILE 
+        echo "remove $CRUN_FILE"
+    fi
+
+    echo "false" > $ARUN_FILE
+    echo "false" > $BRUN_FILE
+    echo "false" > $CRUN_FILE
+
+    echo ${A_NODES[@]}
+    echo ${B_NODES[@]}
+    echo ${C_NODES[@]}
+
+    sbatch _02_sbatch_A.sh $TP $PP $DP $PARTITION
+    sbatch _02_sbatch_B.sh $TP $PP $DP $PARTITION
+    sbatch _02_sbatch_C.sh $TP $PP $DP $PARTITION
+    
+    # waiting if no nodes
+    if $REAPEAT ; then
+        TIMES=3600 # unit: s
+        #HOUR * TIMES = wait time
+        HOUR=1
+        SLEEP_TIME=$((TIMES * HOUR))
+        echo "SLEEP_TIME(s): $SLEEP_TIME"
+        echo "now repeat"
+        while [ true ]
+                do
+                    # check the status of nodes
+                    sleep 100
+
+                    NOW=$(date +"%Y%m%d %H-%M-%S")
+                    echo "waiting: $NOW"
+                    # write each condition of node
+                    ARUN=$(<ARUN.txt)
+                    BRUN=$(<BRUN.txt)
+                    CRUN=$(<CRUN.txt)
+                    
+                    if  ( [ "$ARUN" = "true" ] && [ "$BRUN" = "true" ] && [ "$CRUN" = "true" ] )  ; then
+                        NOW=$(date +"%Y%m%d %H-%M-%S")
+                        echo "waiting: $NOW"
+                        echo "waiting to $SLEEP_TIME hours"
+
+                        ARUN_FILE="ARUN.txt"
+                        BRUN_FILE="BRUN.txt"
+                        CRUN_FILE="CRUN.txt"
+
+                        if [ -e $ARUN_FILE ]; then 
+                            rm $ARUN_FILE 
+                            echo "remove $ARUN_FILE" 
+                        fi
+                        
+                        if [ -e $BRUN_FILE ]; then
+                            rm $BRUN_FILE 
+                            echo "remove $BRUN_FILE"
+                        fi
+
+                        if [ -e $CRUN_FILE ]; then
+                            rm $CRUN_FILE 
+                            echo "remove $CRUN_FILE"
+                        fi
+                    
+                        echo "false" > $ARUN_FILE
+                        echo "false" > $BRUN_FILE
+                        echo "false" > $CRUN_FILE
+                        
+                        sleep $SLEEP_TIME
+                        # save the result
+                        # echo "result saving"
+                        # . result.sh
+                        # sleep 15
+                        # run sbatch
+                        sbatch _02_sbatch_A.sh $TP $PP $DP $PARTITION
+                        sbatch _02_sbatch_B.sh $TP $PP $DP $PARTITION
+                        sbatch _02_sbatch_C.sh $TP $PP $DP $PARTITION
+                        
+                    fi
+                    
+                done
+    fi
+fi
+
+
